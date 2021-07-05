07b422b21e097504971355d18968f244f3ff1144
abhijeet-dhumal <84722973+abhijeet-dhumal@users.noreply.github.com>
2021-07-05 18:54:15 +0530

    Update contact.js

--- contact.js
+++ contact.js
@@ -12,13 +12,14 @@ function register_validate() {
     let uname = document.getElementById("loginInputText");
     let uemail = document.getElementById("exampleInputEmail2");
     let upassword = document.getElementById("exampleInputPassword2");
-    if (uname.value == "abhijeet" && uemail.value == "abhijeet@gmail.com" && upassword="abhi") {
+    if (uname.value == "abhijeet" && uemail.value == "abhijeet@gmail.com" && upassword =="abhi") {
         alert("Welcome Abhijeet!!!");
     }
     else {
-        alert(uname.value +" " + uemail.value +" " + upassword.value + " please enter valid input");
+        alert(uname.value + " " + uemail.value + " " + upassword.value + " please enter valid input");
     }
 }
+
 
 function contact_info() {
     let uname = document.getElementById("contact_name");
