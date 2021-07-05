e137f1fbeb341b351cd657a47c917691768fa7c5
abhijeet-dhumal <84722973+abhijeet-dhumal@users.noreply.github.com>
2021-07-05 18:53:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,10 @@ function register_validate() {
     let uname = document.getElementById("loginInputText");
     let uemail = document.getElementById("exampleInputEmail2");
     let upassword = document.getElementById("exampleInputPassword2");
-    if (uname.value == "abhijeet" && uemail.value == "abhijeet@gmail.com" && upassword="abhi") {
+    if (uname.value == "abhijeet" && uemail.value == "abhijeet@gmail.com" && upassword =="abhi") {
         alert("Welcome Abhijeet!!!");
     }
     else {
-        alert(uname.value +" " + uemail.value +" " + upassword.value + " please enter valid input");
+        alert(uname.value + " " + uemail.value + " " + upassword.value + " please enter valid input");
     }
 }
-
